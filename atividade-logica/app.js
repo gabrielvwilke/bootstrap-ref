c443d941f0a417b57b1c7d0a1091d9009add621a
Gabriel Veras Wilke <gabrielvwilke@gmail.com>
2024-09-08 22:10:56 -0300

    primeiro commit

---
+++ atividade-logica/app.js
@@ -0,0 +1,10 @@
+//Questão 1
+let A = parseFloat(prompt("Digite o valor de A:"));
+let B = parseFloat(prompt("Digite o valor de B:"));
+let C = parseFloat(prompt("Digite o valor de C:"));
+
+if (A + B < C) {
+    console.log("A soma de A + B é menor que C.");
+} else {
+    console.log("A soma de A + B não é menor que C.");
+}
